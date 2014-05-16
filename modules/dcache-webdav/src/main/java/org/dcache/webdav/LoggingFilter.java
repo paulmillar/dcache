09e540786d45743e633a8e409d9aa5e47d09c313
Paul Millar <paul.millar@desy.de>
2014-05-16 08:29:35 +0200

    webdav: switch logging to NetLogger format
    Update WebDAV request logging to match the logging used in SRM and FTP.
    
    Target: master

--- modules/dcache-webdav/src/main/java/org/dcache/webdav/LoggingFilter.java
+++ modules/dcache-webdav/src/main/java/org/dcache/webdav/LoggingFilter.java
@@ -7,26 +7,35 @@ import io.milton.http.Filter;
 import io.milton.http.FilterChain;
 import io.milton.http.Request;
 import io.milton.http.Response;
+import io.milton.http.Response.Status;
 import io.milton.servlet.ServletRequest;
+import io.milton.servlet.ServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.security.auth.Subject;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import java.security.cert.X509Certificate;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+import dmg.cells.nucleus.CDC;
 import org.dcache.auth.Subjects;
+import org.dcache.util.NetLoggerBuilder;
+
+import static org.dcache.webdav.DcacheResourceFactory.TRANSACTION_ATTRIBUTE;
 
 /**
- * Simple request logging for WebDAV door. Interim solution until we
- * switch to a better logging framework with direct support for Jetty.
+ * Request logging for WebDAV door following the NetLogger format. Interim
+ * solution until we switch to a better logging framework with direct support
+ * for Jetty.
  */
 public class LoggingFilter implements Filter
 {
-    private final Logger _log = LoggerFactory.getLogger(LoggingFilter.class);
+    private final Logger ACCESS_LOGGER =
+            LoggerFactory.getLogger("org.dcache.access.webdav");
 
     private static final String X509_CERTIFICATE_ATTRIBUTE =
         "javax.servlet.request.X509Certificate";
@@ -36,57 +45,47 @@ public class LoggingFilter implements Filter
                         final Request request,
                         final Response response)
     {
-        try {
-            filterChain.process(request, response);
+        filterChain.process(request, response);
 
-            Response.Status status = response.getStatus();
-            if (status != null) {
-                _log.info("{} {} {} {} {}",
-                          request.getFromAddress(),
-                          request.getMethod(),
-                          request.getAbsolutePath(),
-                          getUser(request),
-                          status.code);
-            } else {
-                _log.info("{} {} {} {}",
-                          request.getFromAddress(),
-                          request.getMethod(),
-                          request.getAbsolutePath(),
-                          getUser(request));
-            }
-        } catch (RuntimeException e) {
-            _log.warn(String.format("%s %s %s %s",
-                                    request.getFromAddress(),
-                                    request.getMethod(),
-                                    request.getAbsolutePath(),
-                                    getUser(request)),
-                                    e);
-            throw e;
+        NetLoggerBuilder log = new NetLoggerBuilder(getLevel(response),
+                "org.dcache.webdav.request").omitNullValues();
+
+        log.add("request.method", request.getMethod());
+        log.add("request.url", request.getAbsoluteUrl());
+        Status status = response.getStatus();
+        if (status != null) {
+            log.add("response.code", response.getStatus().code);
+            log.add("response.explanation", response.getStatus().text);
+        } else {
+            // Redirection
+            HttpServletResponse servletResponse = ServletResponse.getResponse();
+            log.add("response.code", servletResponse.getStatus());
+            log.add("location", servletResponse.getHeader("Location"));
         }
+        log.add("host.remote", request.getFromAddress());
+        log.add("user-agent", request.getUserAgentHeader());
+
+        log.add("dn", getCertificateName());
+
+        log.add("user", getSubjectName(request));
+        log.add("session", CDC.getSession());
+        log.add("transaction", getTransaction());
+        log.toLogger(ACCESS_LOGGER);
     }
 
-     private String getUser(Request request)
-     {
-         StringBuilder sb = new StringBuilder();
+    private NetLoggerBuilder.Level getLevel(Response response)
+    {
+        Status status = response.getStatus();
 
-         String certificateName = getCertificateName();
-         String subjectName = getSubjectName(request);
-
-         sb.append("[");
-         if(certificateName.isEmpty() && subjectName.isEmpty()) {
-             sb.append("ANONYMOUS");
-         } else {
-             sb.append(certificateName);
-
-             if(!certificateName.isEmpty() && !subjectName.isEmpty()) {
-                 sb.append("; ");
-             }
-
-             sb.append(subjectName);
-         }
-         sb.append("]");
-
-         return sb.toString();
+        if (status == null) {
+            return NetLoggerBuilder.Level.INFO;
+        } else if (status.code >= 500) {
+            return NetLoggerBuilder.Level.ERROR;
+        } else if (status.code >= 400) {
+            return NetLoggerBuilder.Level.WARN;
+        } else {
+            return NetLoggerBuilder.Level.INFO;
+        }
     }
 
     private String getCertificateName()
@@ -104,7 +103,17 @@ public class LoggingFilter implements Filter
             }
         }
 
-        return "";
+        return null;
+    }
+
+    private String getTransaction()
+    {
+        HttpServletRequest servletRequest = ServletRequest.getRequest();
+
+        Object object =
+            servletRequest.getAttribute(TRANSACTION_ATTRIBUTE);
+
+        return object == null ? null : String.valueOf(object);
     }
 
     private String getSubjectName(Request request)
@@ -112,13 +121,13 @@ public class LoggingFilter implements Filter
         Auth auth = request.getAuthorization();
 
         if(auth == null) {
-            return "";
+            return null;
         }
 
         Subject subject = (Subject) auth.getTag();
 
         if(subject == null) {
-            return "";
+            return null;
         }
 
         if(subject.equals(Subjects.NOBODY)) {
